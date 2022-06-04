81627c72dfa8cc8e284fd06b46849cc3bf959acb
BooLightning <99196881+BooLightning@users.noreply.github.com>
2022-06-04 11:01:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import * as THREE from "https://unpkg.com/three@0.126.1/build/three.module.js"
-const scene = new THREE.Scene()
-console.log(scene)
+<script type="importmap">
+  {
+    "imports": {
+      "three": "https://unpkg.com/three@0.126.1/build/three.module.js"
+    }
+  }
+</script>
+
+<script type="module">
+
+  import * as THREE from 'three';
+
+  const scene = new THREE.Scene()
